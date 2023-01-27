859a0fa63bef864df32d2b3ce7e62b2a5e7a32c9
ThaisPerdomo <thaisp@id.uff.br>
2023-01-27 14:06:49 -0300

    catch e finally

--- Requisições/script.js
+++ Requisições/script.js
@@ -6,6 +6,14 @@ function clicou () {
         .then((respostaEmFormaDeObjeto) => {
             console.log(`O título do primeiro post é ${respostaEmFormaDeObjeto[0].title}`)
         })
+        .catch((error) => {
+            alert("Tente novamente mais tarde");
+            console.log(error);
+        })
+        .finally(()=> {
+            console.log("Isso vai executar independente da requisicao ter dado certo ou errado")
+        })
+    
     console.log("Essa mensagem vai aparecer antes da anterior porque a anterior é assíncrona")
 }
 
