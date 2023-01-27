c175477c5565dcf7a3d4ca21e6bbd2e573bf4a2c
ThaisPerdomo <thaisp@id.uff.br>
2023-01-27 13:45:51 -0300

    corrigi o eventlistener

--- Requisições/script.js
+++ Requisições/script.js
@@ -6,7 +6,7 @@ function clicou () {
         .then((respostaEmFormaDeObjeto) => {
             console.log(`O título do primeiro post é ${respostaEmFormaDeObjeto[0].title}`)
         })
-
+    console.log("Essa mensagem vai aparecer antes da anterior porque a anterior é assíncrona")
 }
 
 document.querySelector("#botao").addEventListener('click', clicou);
