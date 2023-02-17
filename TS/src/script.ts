d17d2956514dc8a47d89396d62e318c4fa346a5c
Thais Perdomo <89431979+ThaisPerdomo@users.noreply.github.com>
2023-02-17 16:40:24 -0300

    + ts

--- TS/src/script.ts
+++ TS/src/script.ts
@@ -1,10 +1,16 @@
 var numero1 = document.getElementById('numero1') as HTMLInputElement;
 var numero2 = document.getElementById('numero2') as HTMLInputElement;
-var botao = document.getElementById('somar');
-var resultado = document.getElementById('resultado');
-function calcula(n1, n2) {
+var botao = document.getElementById('somar') as HTMLButtonElement; 
+var resultado = document.getElementById('resultado') as HTMLSpanElement;
+
+function calcula(n1 : number , n2 : number) {
     return n1 + n2;
 }
 botao.addEventListener("click", function () {
-    resultado.innerHTML = calcula(numero1.value, numero2.value);
+    resultado.innerHTML = calcula(+numero1.value, +numero2.value).toString();
 });
+
+
+let aleatoriedades: any = ['João', 'barco'];
+
+aleatoriedades.push(19);
