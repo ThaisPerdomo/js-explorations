8142f3c5359501efdefb4cfd5c90e57cbfdcaed5
Thais Perdomo <89431979+ThaisPerdomo@users.noreply.github.com>
2023-02-21 20:50:34 -0300

    descricao adicionada no metodo get

--- node-explorations/Node/src/server.ts
+++ node-explorations/Node/src/server.ts
@@ -6,4 +6,9 @@ server.get('/', (req: Request, res: Response) => {
     res.send('Hello World');
 });
 
-server.listen(3000);
+// A funcao get tem dois parâmetros e diz respeito a requisicao que vc esta fazendo (neste caso é get; mas tem outra funcao chamada post):
+// O primeiro especifica a rota do site (aqui, de exemplo, é /), o segundo é uma função
+// Essa função pega, também, dois parâmetros req e res (podendo ser qualquer nome na vdd); que foram tipados como request e response (pegados lá do import)
+
+server.listen(3000); 
+// Para criarmos um servidor, devemos utilizar a propriedade listen para ficar "escutando" uma porta, como a 3000.  A padrão para sites html é 80 nao 3000
