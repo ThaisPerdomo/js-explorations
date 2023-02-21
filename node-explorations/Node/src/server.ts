ca3ca2a4819234a13ad2569cd539b8ded4cdb797
Thais Perdomo <89431979+ThaisPerdomo@users.noreply.github.com>
2023-02-21 20:51:10 -0300

    descricao adicionada no metodo get

--- node-explorations/Node/src/server.ts
+++ node-explorations/Node/src/server.ts
@@ -7,7 +7,7 @@ server.get('/', (req: Request, res: Response) => {
 });
 
 // A funcao get tem dois parâmetros e diz respeito a requisicao que vc esta fazendo (neste caso é get; mas tem outra funcao chamada post):
-// O primeiro especifica a rota do site (aqui, de exemplo, é /), o segundo é uma função
+// O primeiro especifica a rota do site (aqui, de exemplo, é /, pq é a pagina inicial), o segundo é uma função
 // Essa função pega, também, dois parâmetros req e res (podendo ser qualquer nome na vdd); que foram tipados como request e response (pegados lá do import)
 
 server.listen(3000); 
